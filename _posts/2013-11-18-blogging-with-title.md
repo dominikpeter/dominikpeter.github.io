580f25e64847f3f8231c218d79e92a18cd2b191a
Dominik Peter <dominik.peter87@gmail.com>
2018-04-08 23:32:12 +0200

    init

--- _posts/2013-11-18-blogging-with-title.md
+++ _posts/2013-11-18-blogging-with-title.md
@@ -1,9 +1,1 @@
----
-layout: post
-title: Blogging with title
-tags: [Test, Ipsum, Markdown, Portfolio]
----
-
-# I am a BIG title
-
-This is a very tiny tiny post with less than 250 letters.
+NA
--- clean-md.R
+++ clean-md.R
@@ -2,17 +2,23 @@
 
 library(readr)
 library(stringr)
+library(purrr)
 
-path <- "_posts/2018-04-08-housing-landscape.md"
+clean_files <- function(path){
+  file <- read_file(path)
+  
+  file_path <- str_extract(file, "\\(../assets/plots/\\w.+png\\)")
+  file_name <- basename(str_extract(file_path, "/\\w.+\\w+"))
+  
+  
+  new_file_path <- paste0('{{ "/assets/plots/',file_name,'" | absolute_url }}')
+  
+  file <- str_replace(file, file_path, new_file_path)
+  
+  file %>% write_file(path)
+}
 
-file <- read_file(path)
 
-file_path <- str_extract(file, "\\(../assets/plots/\\w.+png\\)")
-file_name <- basename(str_extract(file_path, "/\\w.+\\w+"))
+files <- dir("_posts/", full.names = TRUE)
 
-
-new_file_path <- paste0('{{ "/assets/plots/',file_name,'" | absolute_url }}')
-
-file <- str_replace(file, file_path, new_file_path)
-
-file %>% write_file(path)
+walk(files, clean_files)
