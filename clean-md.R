fb6f7a0f65dac8a2933b15ff6087cab048d9e5fb
Dominik Peter <dominik.peter87@gmail.com>
2018-04-09 22:47:20 +0200

    init

--- clean-md.R
+++ clean-md.R
@@ -6,15 +6,10 @@ library(purrr)
 
 clean_files <- function(path){
   file <- read_file(path)
-  
   file_path <- str_extract(file, "\\(../assets/plots/\\w.+png\\)")
   file_name <- basename(str_extract(file_path, "/\\w.+\\w+"))
-  
-  
   new_file_path <- paste0('{{ "/assets/plots/',file_name,'" | absolute_url }}')
-  
   file <- str_replace(file, file_path, new_file_path)
-  
   file %>% write_file(path)
 }
 
