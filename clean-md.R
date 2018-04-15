077ccda753602326fa47f74fafeea78a8e896dfb
Dominik Peter <dominik.peter87@gmail.com>
2018-04-15 18:25:02 +0200

    minor

--- clean-md.R
+++ clean-md.R
@@ -3,19 +3,29 @@
 library(readr)
 library(stringr)
 library(purrr)
+library(tools)
+library(magrittr)
 
 clean_files <- function(path){
   file <- read_file(path)
   file_path <- str_extract(file, "\\(../assets/plots/\\w.+png\\)")
-  file_name <- basename(str_extract(file_path, "/\\w.+\\w+"))
-  new_file_path <- paste0('{{ "/assets/plots/',file_name,'" | absolute_url }}')
-  file <- str_replace(file, file_path, new_file_path)
-  file %>% write_file(path)
+  if (!is.na(file_path)){
+    file_name <- basename(str_extract(file_path, "/\\w.+\\w+"))
+    new_file_path <- paste0('{{ "/assets/plots/',file_name,'" | absolute_url }}')
+    file <- str_replace(file, file_path, new_file_path)
+    file %>% write_file(path)
+  }
 }
 
 
 files <- dir("_posts/", full.names = TRUE)
-files <- files[endsWith(files, ".md")]
-
+files <- files[tools::file_ext(files) == "md"]
 
 walk(files, clean_files)
+
+
+
+
+
+
+
